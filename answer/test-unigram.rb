f1e55397fc58f73cdf600ef6605adb96e2be449f
ShimadaTomoya <tomoya.shimada@naviplus.co.jp>
2020-10-21 09:57:35 +0900

    チュートリアル1の演習問題の回答 http://www.phontron.com/slides/nlp-programming-ja-01-unigramlm.pdf

---
+++ answer/test-unigram.rb
@@ -0,0 +1,68 @@
+require 'bigdecimal'
+require 'bigdecimal/util'
+
+probabilities = {}
+model_file = 'model_file.txt'
+
+File.open(model_file, 'r') do |f|
+  while (line = f.gets) do
+    word, prob = line.split(' ')
+    probabilities[word] = prob.to_f
+  end
+end
+
+=begin
+# 擬似コード（モデル読み込み）
+create a map probabilities
+for each line in model_file
+ split line into w and P
+ set probabilities[w] = P
+=end
+
+lambda1 = 0.95
+lambda_unk = 0.05
+v = 1000000
+w = 0
+h = 0
+unknown = 0
+test_file = '../test/01-test-input.txt'
+
+File.open(test_file, 'r') do |f|
+  words = []
+  while (line = f.gets) do
+    words.push(*line.split(' '))
+    words.push('</s>')
+  end
+  words.each do |word|
+    w += 1
+    p = BigDecimal(lambda_unk.to_s) / BigDecimal(v.to_s)
+    if probabilities[word]
+      p += BigDecimal(lambda1.to_s) * BigDecimal(probabilities[word].to_s)
+    else
+      unknown += 1
+    end
+    h += Math.log(p, 2).abs
+  end
+end
+
+puts "entropy = #{BigDecimal(h.to_s)/BigDecimal(w.to_s)}"
+puts "coverage = #{(w-unknown)/Float(w)}"
+
+
+=begin
+# 擬似コード（評価と結果表示）
+for each line in test_file
+ split line into an array of words
+ append “</s>” to the end of words
+ for each w in words
+  add 1 to W
+  set P = λunk / V
+  if probabilities[w] exists
+    set P += λ1 * probabilities[w]
+  else
+  add 1 to unk
+ add -log2P to H
+
+print “entropy = ”+H/W
+print “coverage = ” + (W-unk)/W
+=end
---
+++ answer/train-unigram.rb
@@ -0,0 +1,39 @@
+count = {}
+total_count = 0
+train_file = '../test/01-train-input.txt' #'/tmp/nlptutorial/data/wiki-en-train.word'
+
+File.open(train_file, 'r') do |f|
+  words = []
+  while (line = f.gets) do
+    words.push(*line.split(' '))
+    words.push('</s>')
+  end
+    words.each do |word|
+      count[word] ? count[word] += 1 : count[word] = 1
+      total_count += 1
+    end
+end
+
+File.open('model_file.txt', 'w', 0755) do |f|
+  count.each do |k,v|
+    probability = Float(v) / Float(total_count)
+    f.print "#{k}\t#{probability}\n"
+  end
+end
+
+=begin
+#擬似コード
+create a map counts
+create a variable total_count = 0
+for each line in the training_file
+ split line into an array of words
+ append “</s>” to the end of words
+ for each word in words
+ add 1 to counts[word]
+ add 1 to total_count
+
+open the model_file for writing
+for each word, count in counts
+ probability = counts[word] / total_count
+ print word, probability to model_file
+=end
